88ccd04c8c39b17ac4601e2facc418ebf9fd0baf
Matt Kilcup <nevermindthelabel@gmail.com>
2019-10-13 16:34:31 -0700

    updates to routes

--- routes/index.js
+++ routes/index.js
@@ -1,54 +1,6 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-// router.use('/api', apiRoutes);
-
-const players = [
-  {
-    firstName: 'Kyler',
-    lastName: 'Murray',
-    team: 'Cardinals',
-    position: 'qb'
-  },
-  {
-    firstName: 'Aaron',
-    lastName: 'Rogers',
-    team: 'Packers',
-    position: 'qb'
-  },
-  {
-    firstName: 'Larry',
-    lastName: 'Fitzgerald',
-    team: 'Cardinals',
-    position: 'wr'
-  }
-];
-
-const teams = [
-  { teamName: 'Cardinals', teamState: 'Arizona', teamConference: 'nfc' },
-  { teamName: 'Packers', teamState: 'Wisconsin', teamConference: 'nfc' }
-];
-
-const db = {
-  teams,
-  players
-};
-
-router.get('/api/:resource', (req, res) => {
-  const resource = req.params.resource;
-  const data = db[resource];
-
-  if (data == null) {
-    res.json({
-      confirmation: 'Fail',
-      message: 'bad request'
-    });
-    return;
-  }
-  res.json({
-    confirmation: 'success',
-    data
-  });
-});
+router.use('/api', apiRoutes);
 
 module.exports = router;
