4b79e614386ef24ce95d37dc748b37767e60db9a
Matt Kilcup <nevermindthelabel@gmail.com>
2019-10-13 21:12:35 -0700

    added update controller

--- controllers/PlayerController.js
+++ controllers/PlayerController.js
@@ -1,7 +1,7 @@
 const Player = require('../models/Players');
 
 module.exports = {
-  get: async params => {
+  get: params => {
     try {
       return new Promise((resolve, reject) => {
         Player.find(params).then(data => {
@@ -13,7 +13,7 @@ module.exports = {
       console.error(error);
     }
   },
-  getById: async id => {
+  getById: id => {
     try {
       return new Promise((resolve, reject) => {
         Player.findById(id).then(data => {
@@ -28,6 +28,17 @@ module.exports = {
   post: params => {
     return new Promise((resolve, reject) => {
       Player.create(params)
+        .then(data => {
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+  put: (id, params) => {
+    return new Promise((resolve, reject) => {
+      Player.findByIdAndUpdate(id, params, {new: true})
         .then(data => {
           resolve(data);
         })
